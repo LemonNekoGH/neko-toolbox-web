8a3583f5b246cf6588d8dd85a011965e892a8b89
LemonNeko <chheese048@gmail.com>
2021-05-16 22:14:27 +0800

    temp commit

--- src/background.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../node_modules/@types/chrome/index.d.ts"/>
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('Hello from the background')
-
-  chrome.tabs.executeScript({
-    file: 'content-script.js'
-  })
-})
